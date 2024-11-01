cdf8756bf5a139628629c8d056304c8bc3a094ee
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-11-01 12:43:30 +0900

    Refactor: 데이터 전처리 로직 리팩토링
    - 쓰레기통 등록 API 리팩토링에 따른 API 요청 방식 변환

--- trashcan_api_client.py
+++ trashcan_api_client.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import requests
+import json
 
 # 엑셀 파일 경로
 input_file = "resultFile/영등포구_담배꽁초_쓰레기통_result_file.xlsx"
 # API URL
 url = "http://localhost:8080/api/trashcan"
+
+# 기본 이미지 파일 경로
+default_image_file_path = "default-trashcan.jpg"  # 여기에 실제 기본 이미지 파일 경로를 입력하세요.
 
 def send_data_to_api():
     # 엑셀 파일 읽기
@@ -20,13 +24,25 @@ def send_data_to_api():
             "longitude": row['longitude']
         }
 
-        try:
+        # JSON 데이터를 문자열로 변환
+        json_data = json.dumps(data)
+
+        # 기본 이미지 파일 준비
+        with open(default_image_file_path, 'rb') as image_file:
             # POST 요청 보내기
-            response = requests.post(url, json=data)
-            response.raise_for_status()  # 요청이 실패하면 예외 발생
-            print(f"데이터 전송 성공: {data}")
-        except requests.exceptions.RequestException as e:
-            print(f"데이터 전송 실패: {data}, 오류: {e}")
+            try:
+                response = requests.post(
+                    url,
+                    files={
+                        'file': (default_image_file_path, image_file, 'image/jpeg'),  # 기본 이미지 파일과 MIME 타입 지정
+                        'data': ('', json_data, 'application/json')  # JSON 문자열을 input으로 전송
+                    },
+
+                )
+                response.raise_for_status()  # 요청이 실패하면 예외 발생
+                print(f"데이터 전송 성공: {data}")
+            except requests.exceptions.RequestException as e:
+                print(f"데이터 전송 실패: {data}, 오류: {e}")
 
 if __name__ == "__main__":
     send_data_to_api()
